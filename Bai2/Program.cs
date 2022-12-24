07786a37dc43fd1a399165ac8982db2261636284
Trung Nguyễn <trungnt1000vn@gmail.com>
2022-12-24 17:41:56 +0700

    update bai2

--- Bai2/Program.cs
+++ Bai2/Program.cs
@@ -10,6 +10,64 @@ namespace Bai2
     {
         static void Main(string[] args)
         {
+
         }
+    }
+   class Person
+    {
+        protected string name;
+
+        public string Name { get => name; set => name = value; }
+    }
+    class Student : Person
+    {
+        private int classnumber;
+
+        public Student()
+        {
+        }
+
+        public int Classnumber { get => classnumber; set => classnumber = value; }
+        public Student(string name,int classnumber)
+        {
+            this.name = name;
+            Classnumber = classnumber;
+        }
+    }
+    class Teacher : Person
+    {
+        public Teacher()
+        {
+        }
+        public Teacher(string name)
+        {
+            this.name = name;
+        }
+    }
+    class Disciplines
+    {
+        private string name;
+        private int lectures;
+        private int exercises;
+
+        public Disciplines()
+        {
+        }
+
+        public string Name { get => name; set => name = value; }
+        public int Lectures { get => lectures; set => lectures = value; }
+        public int Exercises { get => exercises; set => exercises = value; }
+
+    }
+    class Class
+    {
+        List<Student> students = new List<Student>();
+        List<Teacher> teachers = new List<Teacher>();
+        List<Disciplines> disciplines = new List<Disciplines>();
+        Class()
+        {
+
+        }
+
     }
 }
